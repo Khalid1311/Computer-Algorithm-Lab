9348040b774afdfa7b44c50c9dee0a8607fd1bf8
Khalid1311 <khalid1378.bd@gmail.com>
2025-09-02 18:13:26 +0600

    add a meaningful commit message

--- Computer-Algorithm-Lab/src/Lab5/prb3.java
+++ Computer-Algorithm-Lab/src/Lab5/prb3.java
@@ -1,7 +1,25 @@
+/*
+Given an integer n, determine the number of trailing zeroes in the factorial of n (n!).A trailing zero is defined as 
+a zero that appears at the end of a number, after the last non-zero digit. For example,1200 has two trailing zeroes.
+You must design an efficient algorithm that avoids direct computation of n!, since factorial values grow extremely 
+large for even moderate values of n. Instead, focus on analyzing the factors that contribute to trailing zeroes.
 
+Input : 
+A single integer n (1≤n≤10^9)
+
+Output : 
+An integer representing the number of trailing zeroes in n!
+
+Example :
+
+Input :      Output : 
+10           2
+
+Explanation: 10!=3,628,800, which has 2 trailing zeroes.
+*/
 import java.util.Scanner;
 
-public class Prb3 {
+public class prb3 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
